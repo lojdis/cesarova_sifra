534d506cd31bd8930bef42889a9130fcc5130423
janis <113169810+lojdis@users.noreply.github.com>
2023-01-26 10:25:22 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cesarova_sifra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "gaiusjulliusceasar";
+            Console.WriteLine("původní zpráva:{0}", text);
+            string zprava = "";
+            int posun = 1;
+            foreach (char vysledek in text)
+            {
+                int hodnota_ascii = (int)vysledek;
+                hodnota_ascii += posun;
+                if (hodnota_ascii > (int)'z')
+                    hodnota_ascii -= 26;
+                char znak = (char)hodnota_ascii;
+                zprava += znak;
+            }
+            Console.WriteLine("Zašifrovaná zpráva:{0}", zprava);
+            Console.ReadKey();
+        }
+    }
+}
